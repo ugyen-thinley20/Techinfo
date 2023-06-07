21c974c5b644c08b6a418058c6e9d73d703049bb
ugyen-thinley20 <135795517+ugyen-thinley20@users.noreply.github.com>
2023-06-07 12:50:20 +0600

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,10 +1,28 @@
-function hit() {
-    alert("Thank you for your feedback. We will improve according to your feedback");
- }
- 
- const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
  const navbarLinks = document.getElementsByClassName('navbar-links')[0];
  
  toggleButton.addEventListener('click', () => {
      navbarLinks.classList.toggle('active');
  });
+
+ const form = document.querySelector("form");
+ const Btn = document.querySelector(".send-btn");
+ 
+ const allInput = document.querySelectorAll("input");
+ Btn.addEventListener("click", ()=> {
+allInput.forEach(input => {
+ if(input.value != ""){
+     form.classList.add('secActive');
+ }else{
+     form.classList.remove('secActive');
+ }
+});
+function hit() {
+    alert("Thank you for your feedback. We will improve according to your feedback");
+ }
+});
+
+
+
+ 
+ 
